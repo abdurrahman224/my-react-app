ec9aaa2341e99c46fbcc6d2cad7775f9f1eaae2e
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-11-07 22:03:18 +0600

    ser From in data lode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
 import "./App.css";
-import Controlled from "./assets/components/Controlled/Controlled";
-import SubmetFrom from "./assets/components/SubmetFrom/SubmetFrom";
+// import Controlled from "./assets/components/Controlled/Controlled";
+// import SubmetFrom from "./assets/components/SubmetFrom/SubmetFrom";
+import Statefulform from "./components/Statefulform/Statefulform";
 
 function App() {
+
+
+
+const handleSignSubmit =data =>{
+
+
+ console.log("Sign in data:", data);
+}
+const handleRegistSubmit =data =>{
+
+
+ console.log("Register data:", data)
+
+}
+
+
+
+
+
+
   return <>
   
   {/* <SubmetFrom></SubmetFrom> */}
-  <Controlled></Controlled>
+  {/* <Controlled></Controlled> */}
+  <Statefulform  Submit ={"Sign"}  handleSignSubmit={handleSignSubmit}  >
+
+
+<div>
+  <h2>Sign in</h2> 
+  <p>Sign in thes prage</p>
+</div>
+
+  </Statefulform>
+  <Statefulform  Submit={"Regist in"} handleSignSubmit = {handleRegistSubmit}  >
+
+
+    <div>
+      <h2>Regist</h2>
+      <p>Regist new</p>
+         </div>
+  </Statefulform>
 
   
   </>;
